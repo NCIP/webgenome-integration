8fe1e3d31a0dca3ace96d40ed6fb0cf3e65baadf
Ram Bhattaru <bhattarr>
2005-11-15 23:21:01 +0000

    no message
    SVN-Revision: 9738

--- src/gov/nih/nci/caIntegrator/services/appState/ApplicationStateTrackerHome.java
+++ src/gov/nih/nci/caIntegrator/services/appState/ApplicationStateTrackerHome.java
@@ -2,6 +2,9 @@ package gov.nih.nci.caIntegrator.services.appState;
 
 
 
+import gov.nih.nci.caIntegrator.services.appState.ejb.RBTApplicationStateTracker;
+
+import javax.ejb.EJBHome;
 import java.rmi.RemoteException;
 
 /**
@@ -10,6 +13,6 @@ import java.rmi.RemoteException;
  * Date: Sep 20, 2005
  * Time: 9:52:34 AM
  */
-public interface ApplicationStateTrackerHome {
-    ApplicationStateTracker create() throws RemoteException, javax.ejb.CreateException;
+public interface ApplicationStateTrackerHome extends EJBHome {
+    RBTApplicationStateTracker create() throws RemoteException, javax.ejb.CreateException;
 }
